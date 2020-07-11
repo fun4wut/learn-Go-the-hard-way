6fb07e8b447fbce99229d3ab741940636627b266
fun4wut <nihouze@163.com>
2020-07-11 14:02:05 +0800

    l1 fin

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"reflect"
+)
+
 //Reverse reverses a slice.
-var Reverse func(slice interface{})
+var Reverse func(slice interface{}) = func(slice interface{}) {
+	var t = reflect.ValueOf(slice).Elem()  // 使用Elem获取指针所指的元素
+	swap := reflect.Swapper(t.Interface()) // 使用Interface获取对应的值
+	for i, j := 0, t.Len()-1; i < j; {
+		swap(i, j)
+		i++
+		j--
+	}
+}
 
 func main() {
 	println("Please edit main.go,and complete the 'Reverse' function to pass the test.\nYou should use reflect package to reflect the slice type and make it applly to any type.\nTo run test,please run 'go test'\nIf you pass the test,please run 'git checkout l2' ")
