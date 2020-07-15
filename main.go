1fc506cd2210f67bf918a34058a5e6b41f1cbf82
Houze Ni <t-hni@microsoft.com>
2020-07-15 13:32:58 +0800

    就这?

--- main.go
+++ main.go
@@ -28,14 +28,17 @@ type Middleware interface {
 
 //TODO:Use add a new Middleware that implements Handle(*Context)
 func (s *Server) Use(middlewares ...Middleware) {
+	s.middlewares = append(s.middlewares, middlewares...)
 }
 
 //TODO:Next calls next middleware.
 func (ctx *Context) Next() {
+	ctx.idx += 1
 }
 
 //TODO:Invok calls middleware at index of ctx.idx.
 func (ctx *Context) Invok() {
+	ctx.middlewares[ctx.idx].Handle(ctx)
 }
 
 //implements http.Handle
